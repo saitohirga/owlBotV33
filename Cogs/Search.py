b7ffb367c03b40fd5e25d5eb746ebe3c8af03323
Grant B <2722492+saitohirga@users.noreply.github.com>
2023-11-12 14:34:54 -0500

    Update Search.py

--- Cogs/Search.py
+++ Cogs/Search.py
@@ -1,24 +1,24 @@
-from discord.ext import commands
-import lxml
-from Utilities.Scraper import Scraper
-from Utilities.Logging import autoLog
+#from discord.ext import commands
+#import lxml
+#from Utilities.Scraper import Scraper
+#from Utilities.Logging import autoLog
 
 
-class Search(commands.Cog):
+#class Search(commands.Cog):
+#
+#   # attaches class of commands to existing client in main
+#    def __init__(self, client):
+#        self.scraper = Scraper()
+#        self.client = client
+#
+#    @commands.Cog.listener()
+#    async def on_ready(self):
+#        autoLog('Search cog online', 'okgreen')
+#    
+#    @commands.command()
+#   async def search(self, ctx, search: str = None):
+#       # this calls a Scraper member function, which also initializes and calls a Sitemap class member function
+#       self.scraper.sitemapSearch(search)
 
-    # attaches class of commands to existing client in main
-    def __init__(self, client):
-        self.scraper = Scraper()
-        self.client = client
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        autoLog('Search cog online', 'okgreen')
-    
-    @commands.command()
-    async def search(self, ctx, search: str = None):
-        # this calls a Scraper member function, which also initializes and calls a Sitemap class member function
-       self.scraper.sitemapSearch(search)
-
-def setup(client):
-    client.add_cog(Search(client))
+#def setup(client):
+#    client.add_cog(Search(client))
