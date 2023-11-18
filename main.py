35f148e0281bc25c039280575b72b403c9fc381f
Grant B <2722492+saitohirga@users.noreply.github.com>
2023-11-17 23:16:16 -0500

    MAYBE?

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord import app_commands
 from time import gmtime, strftime
 import datetime
 import data.key
-
+from discord.ext import commands
 
 MY_GUILD = discord.Object(id=458765854624972811)  # replace with your guild id
 
@@ -39,22 +39,30 @@ async def rat(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hello  {interaction.user.mention}, No rats spotted in the caf as of today, if this changes DM Saito, time since " f"last seen {diff}")
 
 
-@discord.app_commands.command(description="Echoes the message to a specified channel")
-@discord.app_commands.describe(message="The message to echo")
-async def secret_echo(self, interaction: discord.Interaction, message: str):
-    role_id = 882444308857384991  # Your specific role ID
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Correctly defining the slash command within the cog
+    @discord.app_commands.command(name="secret_echo", description="Echoes the message to a specified channel")
+    @discord.app_commands.describe(message="The message to echo")
+    async def secret_echo(self, interaction: discord.Interaction, message: str):
+        role_id = 882444308857384991  # Your specific role ID
 
         # Check if the user has the required role
-    if discord.utils.get(interaction.user.roles, id=role_id):
+        if discord.utils.get(interaction.user.roles, id=role_id):
             # Replace 'your_channel_id' with the ID of the channel you want the message to be sent to
-        channel = self.bot.get_channel(458765855115968513)
-        if channel:
-            await channel.send(message)
-            await interaction.response.send_message(f"Message sent to {channel.mention}", ephemeral=True)
+            channel = self.bot.get_channel(458765855115968513)
+            if channel:
+                await channel.send(message)
+                await interaction.response.send_message(f"Message sent to {channel.mention}", ephemeral=True)
+            else:
+                await interaction.response.send_message("Channel not found.", ephemeral=True)
         else:
-            await interaction.response.send_message("Channel not found.", ephemeral=True)
-    else:
-        await interaction.response.send_message("You don't have permission to use this command.", ephemeral=True)
+            await interaction.response.send_message("You don't have permission to use this command.", ephemeral=True)
+
+# Add the cog to the bot
+client.add_cog(MyCog(client))
 
 
    
