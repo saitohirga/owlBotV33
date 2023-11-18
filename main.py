5610c5af72ca3b7e2857e3f5bd4a9e7735b9a70f
Grant B <2722492+saitohirga@users.noreply.github.com>
2023-11-17 23:32:06 -0500

    BACK IN TIME

--- main.py
+++ main.py
@@ -5,24 +5,23 @@ from discord import app_commands
 from time import gmtime, strftime
 import datetime
 import data.key
-from discord.ext import commands
+
 
 MY_GUILD = discord.Object(id=458765854624972811)  # replace with your guild id
 
 
-class MyClient(commands.Bot):
+class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
-        super().__init__(command_prefix="!", intents=intents)
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
 
+    
     async def setup_hook(self):
-        # Await the add_cog call
-        await self.add_cog(MyCog(self))
-        # Sync the commands with the guild
+        self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
-# Enable the message content intent
+
 intents = discord.Intents.default()
-intents.message_content = True
 client = MyClient(intents=intents)
 
 @client.event
@@ -37,35 +36,6 @@ async def rat(interaction: discord.Interaction):
     date2 = '2019-11-18 12:25:34'
     date1 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
-    await interaction.response.send_message(f"Hello  {interaction.user.mention}, No rats spotted in the caf as of today, if this changes DM Saito, time since " f"last seen {diff}")
-
-
-class MyCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    # Correctly defining the slash command within the cog
-    @discord.app_commands.command(name="secret_echo", description="Echoes the message to a specified channel")
-    @discord.app_commands.describe(message="The message to echo")
-    async def secret_echo(self, interaction: discord.Interaction, message: str):
-        role_id = 882444308857384991  # Your specific role ID
-
-        # Check if the user has the required role
-        if discord.utils.get(interaction.user.roles, id=role_id):
-            # Replace 'your_channel_id' with the ID of the channel you want the message to be sent to
-            channel = self.bot.get_channel(458765855115968513)
-            if channel:
-                await channel.send(message)
-                await interaction.response.send_message(f"Message sent to {channel.mention}", ephemeral=True)
-            else:
-                await interaction.response.send_message("Channel not found.", ephemeral=True)
-        else:
-            await interaction.response.send_message("You don't have permission to use this command.", ephemeral=True)
-
-# Add the cog to the bot
-client.add_cog(MyCog(client))
-
-
-   
+    await interaction.response.send_message(f"Hello  {interaction.user.mention}, No rats spotted in the caf as of today, if this changes DM Saito, time since " f"last seen {diff}")   
 
 client.run(data.key.token)                                          
