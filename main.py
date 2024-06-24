5b01b15ac8924817ee77c2bd87409586c1f062f1
Grant B <2722492+saitohirga@users.noreply.github.com>
2024-06-23 20:56:45 -0400

    lets break things with portals

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from typing import Optional
-
 import discord
 from discord import app_commands
 from time import gmtime, strftime
@@ -14,15 +13,12 @@ class MyClient(discord.Client):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
 
-    
     async def setup_hook(self):
         self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
-
 intents = discord.Intents.default()
 client = MyClient(intents=intents)
-
 
 @client.event
 async def on_ready():
@@ -36,26 +32,26 @@ async def rat(interaction: discord.Interaction):
     date2 = '2019-11-18 12:25:34'
     date1 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
-    msg = f"Hello {interaction.user.mention}, No rats spotted in the caf as of today, if this changes DM Saito, time since " f"last seen {diff}"
-    await interaction.response.defer(msg, ephemeral=True, thinking=True)
+    msg = f"Hello {interaction.user.mention}, No rats spotted in the caf as of today, if this changes DM Saito, time since last seen {diff}"
+    await interaction.response.defer(thinking=True)
     await asyncio.sleep(30)
     await interaction.followup.send(msg)
 
 @client.tree.command()
 async def bird(interaction: discord.Interaction):
-     """Checks for birds"""
-     datetimeFormat = '%Y %m %d %H:%M:%S'
-     date2 = '2021 10 20 7:44:36'
-     date1 = strftime("%Y %m %d %H:%M:%S", gmtime())
-     diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
-     await interaction.response.send_message(f"Hello {interaction.user.mention}, No bird spotted in the caf as of today, if this changes DM Saito, time since last seen {diff}")
+    """Checks for birds"""
+    datetimeFormat = '%Y-%m-%d %H:%M:%S'
+    date2 = '2021-10-20 07:44:36'
+    date1 = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    diff = datetime.datetime.strptime(date1, datetimeFormat) - datetime.datetime.strptime(date2, datetimeFormat)
+    await interaction.response.send_message(f"Hello {interaction.user.mention}, No bird spotted in the caf as of today, if this changes DM Saito, time since last seen {diff}")
 
 @client.tree.command()
 @app_commands.rename(text_to_send='text')
 @app_commands.describe(text_to_send='reports on what our owl is thinking')
 @app_commands.default_permissions(manage_messages=True)
 async def owlthought(interaction: discord.Interaction, text_to_send: str):
-    """reports what our owl is thinking"""
+    """Reports what our owl is thinking"""
     await interaction.response.send_message(text_to_send)
 
-client.run(data.key.token)                                          
+client.run(data.key.token)
